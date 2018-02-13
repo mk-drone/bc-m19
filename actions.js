837b9cfdbc54c7e43ec392f4366682c8c9af8792
mk <mk@opinion.com.pl>
2018-02-13 18:30:47 +0100

    Initial commit

---
+++ actions.js
@@ -0,0 +1,38 @@
+
+
+const addComment = (text) => {
+    return {
+        type: 'ADD_COMMENT',
+        text: text,
+        id: uuid.v4(),
+    };
+}
+
+const editComment = (id, text) => {
+    return {
+        type: 'EDIT_COMMENT',
+        text: text,
+        id: id,
+    }
+}
+
+const deleteComment = (id) => {
+    return {
+        type: 'DELETE_COMMENT',
+        id: id,
+    };
+}
+
+const thumbUp = (id) => {
+    return {
+        type: 'DELETE_COMMENT',
+        id: id,
+    };
+}
+
+const thumbDown = (id) => {
+    return {
+        type: 'DELETE_COMMENT',
+        id: id,
+    };
+}
